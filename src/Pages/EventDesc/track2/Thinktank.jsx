be0d3af36cf86be8e4d1a505ca846069261b7346
dhanalakshmim-eng <dd406652dhana@gmail.com>
2025-09-17 12:36:36 +0530

    my changes

--- src/Pages/EventDesc/track2/Thinktank.jsx
+++ src/Pages/EventDesc/track2/Thinktank.jsx
@@ -29,7 +29,7 @@ const Thinktank = () => {
       >
         <img
           src={EventImage}
-          alt="Event"
+          alt="Thinktank Event"
           className="w-3/4 md:w-2/3 lg:w-2/3 object-cover rounded-lg shadow-lg"
         />
       </motion.div>
@@ -45,7 +45,7 @@ const Thinktank = () => {
         <h1>
           ✨
           <span className="text-head lg:text-4xl font-bold mb-4 text-center lg:text-left gradient-text">
-            SynaptiQ: Business Knowledge Event
+            Thinktank: Business Knowledge Event
           </span>
         </h1>
 
