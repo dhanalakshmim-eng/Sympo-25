2bc530f4d147f06574e2cb4bce894857a62279c2
KishorJJ11 <kishorjj05@gmail.com>
2025-09-18 10:29:24 +0530

    test

--- src/Sections/CollegeName.jsx
+++ src/Sections/CollegeName.jsx
@@ -5,7 +5,7 @@ import 'animate.css';
 const CollegeName = () => {
   return (
     <div className="text-center p-4 sm:p-6 md:p-8 lg:p-6 xl:p-6">
-      <p className=" clg-font  bg-blue-200 text-3xl sm:text-2xl md:text-3xl lg:text-5xl   font-extrabold mb-0 bg-white bg-clip-text text-transparent font-otherfont animate__animated animate__fadeInUp duration-500">
+      <p className="mt-20 clg-font  bg-blue-200 text-3xl sm:text-2xl md:text-3xl lg:text-5xl   font-extrabold mb-0 bg-white bg-clip-text text-transparent font-otherfont animate__animated animate__fadeInUp duration-500">
         JERUSALEM COLLEGE OF ENGINEERING <br/>
         <span className="clg-font  bg-blue-200  text-xl font-extrabold bg-white bg-clip-text text-transparent font-otherfont animate__animated animate__fadeInUp duration-500">AN AUTONOMOUS INSTITUTION</span><br />
         <span className="clg-font  bg-blue-200  text-sm sm:text-sm md:text-base lg:text-lg font-extrabold bg-white bg-clip-text text-transparent font-otherfont animate__animated animate__fadeInUp duration-500">Velachery Main Road, Narayanapuram, Pallikaranai, Chennai, Tamil Nadu - 600100</span><br />
